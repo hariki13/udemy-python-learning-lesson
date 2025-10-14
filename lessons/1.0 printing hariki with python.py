bf9c85e0bee15bed60e437807f69705c391f42e1
hariki13 <sadewa302@gmail.com>
2025-10-14 18:30:53 +0000

    added print lesson and exercise

---
+++ lessons/1.0 printing hariki with python.py
@@ -0,0 +1,16 @@
+print("hello,world!")
+print("i'm learn python!")
+print("this is so much fun!")
+print("this is so much fun!")
+print("i cant wait to learn more!")
+
+print('this is a string with single quotes.')
+print("this is a string with double quotes.")
+
+print("he said, 'python is awesome!'")
+print('he replied,"i totally agree!"')
+
+print("first line\nsecondline")
+print("this\tis\ttabbed")
+
+print("my name is","hariki","and i am","32","years","old")
