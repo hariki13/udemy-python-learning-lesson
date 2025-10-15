1cbe7eacb799e70f1617288734624790a4e52b8e
hariki13 <sadewa302@gmail.com>
2025-10-15 10:40:33 +0000

    added lesson1_print_basic_string_solution

--- exercises/lesson1_print_basic_string by hariki.py
+++ exercises/lesson1_print_basic_string by hariki.py
@@ -1,12 +1,24 @@
 # 1. print a short story using multiple lines
 print("story of python")
+print("--------------")
+
+print("python was created by Guido van Rossum")
+print("it was first released in 1991.")
+
+print("\npython version:")
+print("python 1.0 - 1994")
+print("python 2.0 - 2000")
+print("python 3.0 - 2008")
+
 
 # 2. create a string that includes both single and double quotes.
-#TODO:
+print('Guido van Rossum once said, "python is an experiment is how much freedom programmers need. too much freedom and nobody can read another\'s code; too little and expressiveness is endangered"')
+
 
 # 3. use escape characters to create a formatted list.
-#TODO:
+print("the Zen of python includes:\n\t1. beautiful is better than ugly.\n\t2. explicit is better than implicit.\n\t3. simple is better complex.\n\t4. complex is better than complicated.\n\t5. readability counts.")
+
 
 # 4. combine different type of data (strings and numbers) in one print statement
-#TODO:
+print("python was released in", 1991)
 
