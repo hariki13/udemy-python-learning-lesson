a0d06549d83e29101979e9509308b4fce6229d35
hariki13 <sadewa302@gmail.com>
2025-10-15 10:17:27 +0000

    added  print lesson and exercise

---
+++ exercises/lesson1_print_basic_string by hariki.py
@@ -0,0 +1,12 @@
+# 1. print a short story using multiple lines
+print("story of python")
+
+# 2. create a string that includes both single and double quotes.
+#TODO:
+
+# 3. use escape characters to create a formatted list.
+#TODO:
+
+# 4. combine different type of data (strings and numbers) in one print statement
+#TODO:
+
